2b03c38ca55ef3e650d17ff8520fc7aa582d90d8
alcachofa96 <sofiameza1296@gmail.com>
2025-08-06 18:57:43 -0600

    Se corrigieron validaciones

--- contacto.js
+++ contacto.js
@@ -1,29 +1,33 @@
-const contactoForm = document.getElementById('contactoForm');
-const input = document.querySelector('#contactoForm input');
+// const contactoForm = document.getElementById('contactoForm');
+// const input = document.querySelector('#contactoForm input');
+const textoMensaje = document.getElementById("mensaje");
 
 export default function validarNombre(nombre){
     const regex = new RegExp("[a-zA-Z0-9_\s]");
     return regex.test(nombre);
 }
- export function validarApellido(apellido){
+
+export function validarApellido(apellido){
     const regex = new RegExp("[a-zA-Z0-9_\s]");
     return regex.test(apellido);
- }
+}
+
 export  function validarEmail(email){
-   const regex = new RegExp("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+");
-   return regex.test(email);
+    const regex = new RegExp("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+");
+    return regex.test(email);
 }
 
 export function validarTelefono(telefono){
-    const regex = new RegExp("[^\+[1-9]\d{1,14}$]");
+    const regex = new RegExp("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$");
     return regex.test(telefono);
 }
 
-export function validarMensaje(mensaje){
-    if (isNaN(mensaje)) {
+
+export function validarMensaje() {
+    if (textoMensaje.value.trim() === "") {
+        alert("Por favor escribe un mensaje.");
         return false;
     }
-    const num = Number(mensaje);
-    return num > 10 && num <= 100;
+    return true;
 }
 
--- contactomain.js
+++ contactomain.js
@@ -1,23 +1,54 @@
 import validarNombre from "./contacto.js";
-import { validarApellido,validarEmail, validarTelefono, validarMensaje } from "./contacto.js";
+import { validarApellido, validarEmail, validarTelefono, validarMensaje } from "./contacto.js";
 
-const exampleFormControlInput1 = document.getElementById("exampleFormControlInput1");
-const exampleFormControlInput2 = document.getElementById("exampleFormControlInput2");
-const exampleFormControlInput3 = document.getElementById("exampleFormControlInput3");
-const exampleFormControlInput4 = document.getElementById("exampleFormControlInput4");
-const exampleFormControlTextarea5 = document.getElementById("exampleFormControlTextarea5");
-
+const form = document.getElementById("contactoForm");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const telefono = document.getElementById("telefono");
+const mensaje = document.getElementById("mensaje");
 const boton = document.getElementById("boton");
-
 
 boton.addEventListener("click", function(event){
     event.preventDefault();
-  
-    alertResultado.innerHTML = ""; 
-    alertResultado.innerHTML += "Revisa tu nombre: " + validarNombre(exampleFormControlInput1.value) + "</br>";
-    alertResultado.innerHTML += "Revisa tu nombre: " + validarApellido(exampleFormControlInput2.value) + "</br>";
-    alertResultado.innerHTML += "Revisa tu Email: " + validarEmail(exampleFormControlInput3.value) + "</br>";
-    alertResultado.innerHTML += "Revisa tu Telefono: " + validarTelefono(exampleFormControlInput4.value) + "</br>";
-    alertResultado.innerHTML += "Mensaje no valido: " + validarMensaje(exampleFormControlTextarea5.value) + "</br>";
+    alertResultado.innerHTML = "Revisa tu nombre: " + validarNombre(nombre.value) + "</br>";
+    alertResultado.innerHTML += "Revisa tu apellido: " + validarApellido(apellido.value) + "</br>";
+    alertResultado.innerHTML += "Revisa tu Email: " + validarEmail(email.value) + "</br>";
+    alertResultado.innerHTML += "Revisa tu Telefono: " + validarTelefono(telefono.value) + "</br>";
+    alertResultado.innerHTML += "Mensaje no valido: " + validarMensaje(mensaje.value) + "</br>";
 });
+
+
+// EMAIL JS------
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Evita el envío por defecto
+
+if (validarFormularioCompleto()) {
+// Enviar con EmailJS
+    emailjs.sendForm("service_c04p4z4", "template_hjtdd87", form)
+    .then(
+        function (response) {
+        console.log(
+            "Correo enviado con éxito",
+            response.status,
+            response.text
+          );
+          alert("✅ Formulario enviado correctamente.");
+          form.reset();
+          limpiarErrores();
+        },
+        function (error) {
+          console.error("Error al enviar el correo", error);
+          alert(
+            "❌ Ocurrió un error al enviar el formulario. Intenta más tarde."
+          );
+        }
+      );
+
+} else {
+    console.log("El formulario no es válido. Por favor, corrige los errores.");
+  }
+});
+
 
